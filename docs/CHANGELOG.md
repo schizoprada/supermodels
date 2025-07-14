0b88803963526ed3704fa8fb37eae10eac6e4127
schizoprada <150098802+schizoprada@users.noreply.github.com>
2025-07-14 12:08:23 -0400

    initial commit

---
+++ docs/CHANGELOG.md
@@ -0,0 +1,4 @@
+# SUPERMODELS - CHANGELOG
+
+## [0.1.0] -- *07/14/2025*
+* Project Initialized
---
+++ src/supermodels/__init__.py
@@ -0,0 +1,14 @@
+# ~/supermodels/src/supermodels/__init__.py
+"""
+supermodels
+-----------
+...
+"""
+
+__version__ = "0.1.0"
+__author__ = "Joel Yisrael"
+__email__ = "schizoprada@gmail.com"
+__license__ = "MIT"
+__url__ = "https://github.com/schizoprada/supermodels"
+
+VERSION = tuple(map(int, __version__.split('.')))
